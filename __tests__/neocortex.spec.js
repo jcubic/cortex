5185a409bed7b24dd3eff7ccd3ea5ca11d92fcaf
Jakub Jankiewicz <jcubic@onet.pl>
2024-10-10 01:28:33 +0200

    more tests

--- __tests__/neocortex.spec.js
+++ __tests__/neocortex.spec.js
@@ -1,7 +1,9 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 
 import * as acorn from 'acorn';
 import Interpreter from '../interpreter';
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 Interpreter.nativeGlobal.acorn = acorn;
 
@@ -25,3 +27,45 @@ Interpreter.nativeGlobal.acorn = acorn;
         expect(inter.run()).toBe(false);
     });
 }
+
+test('it should create external function', () => {
+    const fn = vi.fn();
+    const args = ['hello', 'world'];
+    const code = `alert.apply(null, ${JSON.stringify(args)});`;
+    const inter = new Interpreter(code, (interpreter, globalObject) => {
+        const native_fn = interpreter.createNativeFunction(fn);
+        interpreter.setProperty(globalObject, 'alert', native_fn);
+    });
+    inter.run();
+    expect(fn.mock.calls[0]).toEqual(['hello', 'world']);
+});
+
+test('it should create object and method', () => {
+    const fn = vi.fn();
+    const code = 'console.log("hello, world!")';
+    const inter = new Interpreter(code, (interpreter, globalObject) => {
+        const console = interpreter.nativeToPseudo({});
+        interpreter.setProperty(globalObject, 'console', console);
+        const native_fn = interpreter.createNativeFunction(fn);
+        interpreter.setProperty(console, 'log', native_fn);
+    });
+    inter.run();
+    expect(fn.mock.calls[0]).toEqual(['hello, world!']);
+});
+
+test('it should create and evaluate async task', async () => {
+    const code = 'setTimeout(function() { alert("hello"); }, 0)';
+    const fn = vi.fn();
+    const inter = new Interpreter(code, (interpreter, globalObject) => {
+        const native_fn = interpreter.createNativeFunction(fn);
+        interpreter.setProperty(globalObject, 'alert', native_fn);
+    });
+    (async () => {
+        while (inter.step()) {
+            await Promise.resolve();
+        }
+    })();
+    expect(fn.mock.calls.length).toEqual(0);
+    await delay(10);
+    expect(fn.mock.calls[0]).toEqual(['hello']);
+});
--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,8 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'lcov']
+      reporter: ['text', 'json', 'lcov'],
+      exclude: ['./demos/*', '**/*.spec.js', 'vitest.config.js']
     }
   }
 });
